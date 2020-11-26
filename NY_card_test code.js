93122ae351eb2e3b90340b6bb9d328fe853519f1
ibykova <bykova92@gmail.com>
2020-11-26 23:27:48 +0200

    initial commit

---
+++ NY_card_test code.js
@@ -0,0 +1,16 @@
+let year = 2021;
+
+const message = "З Новим Роком та Різдвом Христовим! ";
+
+function greet(msg) {
+
+  if (year > 2020) {
+
+    console.log(msg + "Найкращі побажання у Новому 2021 Році!");
+
+  }
+}
+
+greet(message);
+
+
